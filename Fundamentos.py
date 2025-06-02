e879fb77841278fa0d5d92b5c3d2265312825220
JLHS010728 <jorgesida31@gmail.com>
2025-06-02 11:23:35 -0600

    Create Fundamentos.py
    Se vio el comportamiento de los operadores aritméticos, lógicos y de comparación.

---
+++ Fundamentos.py
@@ -0,0 +1,33 @@
+if __name__ == "__main__":
+
+    print("-----Operadores aritmeticos-----")
+    print ( "Operador suma, 5 + 6 =" , 5 + 6 ) 
+    print("Operador resta, 9 - 8 =", 9 - 8)
+    print("Operador multiplicacion, 9 * 8 =", 9 * 8)
+    print("Operador division 9 / 8 =", 9 / 8)
+    print("Operador division unitaria 9 / 8 =", 9 / 8)
+    print("Operador exponente 9 ** 8 =", 9 ** 8)
+
+    #Este es un comentario de una sola linea.
+    '''Este es un comentario 
+    de varias lineas'''
+
+    print("-----Operadores Logicos-----")
+    print("True and True =", True and True)
+    print("True and False =", True and False)
+    print("False and True =", False and True)
+    print("False and False =", False and False)
+    print("True or True =", True or True)
+    print("True or False =", True or False)
+    print("False or True =", False or True)
+    print("False or False =", False or False)
+    print("Not True =", not True)
+    print("Not False =", not False)
+
+    print("-----Operadores de comparacion-----")
+    print("Es igual que, 5 == 5 =", 5 == 5)
+    print("Es distinto que, 5 != 5 =", 5 != 5)
+    print("Es menor que. 5 < 5 =", 5 < 5)
+    print("Es menor o igual que, 5 <= 5 =", 5 <= 5)
+    print("Es mayor que, 5 > 5 =", 5 > 5)
+    print("Es mayor o igual que, 5 >= 5 =", 5 >= 5)
